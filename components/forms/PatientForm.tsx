6635c3a4940ccac450288c6eee072c6ef5f30c6b
nipunac99 <nipunachamod99723@gmail.com>
2024-07-12 01:47:15 +0530

    submit button and home page design

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -6,24 +6,39 @@ import { z } from "zod";
 import { Button } from "@/components/ui/button";
 import { Form } from "@/components/ui/form";
 import CustomFormField, { FormFieldType } from "../CustomFormField";
+import SubmitButton from "../SubmitButton";
+import { useState } from "react";
+import { UserFormValidation } from "@/lib/validation";
+import { useRouter } from "next/navigation";
 
 
 
-const formSchema = z.object({
-  username: z.string().min(2, {
-    message: "Username must be at least 2 characters.",
-  }),
-});
+
 const PatientForm = () => {
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
+    const router = useRouter
+    const [isLoading, setIsLoading] = useState(false);
+
+  const form = useForm<z.infer<typeof UserFormValidation>>({
+    resolver: zodResolver(UserFormValidation),
     defaultValues: {
-      username: "",
+      name: "",
+      email: "",
+      phone: ""
     },
   });
 
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    console.log(values);
+  async function onSubmit({name, email, phone}: z.infer<typeof UserFormValidation>) {
+    setIsLoading(true);
+
+    try{
+        // const userdata ={name, email, phone};
+
+        // const user = await createUser(userdata);
+
+        // if(user) router.push(`/patients/${user.$id}/register`)
+
+    }catch (error) {
+console.log(error)};
   }
   return (
     <Form {...form}>
@@ -61,7 +76,7 @@ const PatientForm = () => {
           placeholder="(+94) 123-4567"
         />
 
-        <Button type="submit">Submit</Button>
+        <SubmitButton isLoading={isLoading}>Get Started</SubmitButton>
       </form>
     </Form>
   );
